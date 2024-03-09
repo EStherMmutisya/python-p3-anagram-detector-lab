04516b80b53460cdc6670548e1ac3ff4d3180189
EStherMmutisya <starjacob994@gmail.com>
2024-03-09 08:02:07 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+
+
+class Anagram :
+    def __init__(self, word):
+
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        return [anagram for anagram in word_list if sorted(anagram) == sorted_word]
